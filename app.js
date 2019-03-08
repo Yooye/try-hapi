2c78eef223129541a925624af078d7f21ee43b87
yooye-company <luyy_cq@hqyj.com>
2019-03-08 16:46:01 +0800

    Fixed a problem that the server url like htttp://DESKTOP-28AU4HV:6951

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const Hapi = require('hapi');
+require('env2')('./.env');  //【问题】必须放在config前面，否则提供的服务地址会为类似http://DESKTOP-28AU4HV:65169
 const config = require('./config');
 const routesHelloHapi = require('./routes/hello-hapi.js');
 const routesShops = require('./routes/shops');
 const routesOrders = require('./routes/orders');
-require('env2')('./.env');
 const pluginHapiSwagger = require('./plugins/hapi-swagger'); //引入swagger配置模块
 const server =new Hapi.Server();
 
